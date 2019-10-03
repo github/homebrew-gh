d472795ef42c9bc72796f98162f7cc7887c1bd0d
Mislav Marohnić <mislav@github.com>
2019-10-03 15:41:04 +0200

    Create gh.rb

---
+++ Formula/gh.rb
@@ -0,0 +1,16 @@
+class Gh < Formula
+  desc "GitHub CLI"
+  head "https://github.com/github/github-cli-prototype.git", :branch => "prototype"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-mod=vendor", "-o", "gh", "."
+    bin.install "gh"
+  end
+
+  test do
+    help_text = shell_output("#{bin}/gh --help")
+    assert_includes "Usage:", help_text
+  end
+end
